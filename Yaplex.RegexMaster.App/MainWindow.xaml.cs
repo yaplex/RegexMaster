f98c4002db51a90a97e91bf34b24dde152a9609f
Alex Shapovalov <alex@yaplex.com>
2014-02-14 10:25:09 -0500

    something works

--- Yaplex.RegexMaster.App/MainWindow.xaml.cs
+++ Yaplex.RegexMaster.App/MainWindow.xaml.cs
@@ -1,28 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Yaplex.RegexMaster.App
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RunRegex();
+        }
+
+        private void RunRegex()
+        {
+            if (null == RichTextToParce) return;
+
+            string regexPattern = TxtPattern.Text;
+            FlowDocument originalDoc = RichTextToParce.Document;
+            string content = new TextRange(originalDoc.ContentStart, originalDoc.ContentEnd).Text;
+            var selectionDoc = new FlowDocument();
+
+            Match m = Regex.Match(content,
+                regexPattern);
+
+            while (m.Success)
+            {
+                BuildSelection(content, selectionDoc, m);
+                m = m.NextMatch();
+            }
+
+            RichTextToParce.Document = selectionDoc;
+        }
+
+        private void BuildSelection(string content, FlowDocument selectionDoc, Match m)
+        {
+            var p = new Paragraph();
+            p.Inlines.Add(new Run(content.Substring(0, m.Index)));
+            p.Inlines.Add(new Bold(new Run(content.Substring(m.Index, m.Length))));
+            p.Inlines.Add(new Run(content.Substring(m.Index+m.Length, content.Length-1-m.Index-m.Length)));
+
+            selectionDoc.Blocks.Add(p);
+
         }
     }
 }
---
+++ Yaplex.RegexMaster.Business/PresentationViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Documents;
+using Yaplex.RegexMaster.Business.Annotations;
+
+namespace Yaplex.RegexMaster.Business
+{
+    public class PresentationViewModel : INotifyPropertyChanged
+    {
+        private string _regexPattern;
+
+        public string RegexPattern
+        {
+            get { return _regexPattern; }
+            set
+            {
+                if (value == _regexPattern) return;
+                _regexPattern = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public FlowDocument Document { get; set; }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
