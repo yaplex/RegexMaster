10f29fca945fb1bfe45a27c3b1469dd81c7e926e
Alex Shapovalov <alex@yaplex.com>
2014-02-20 14:32:16 -0500

    unit test

--- Yaplex.RegexMaster.Tests/Class1.cs
+++ Yaplex.RegexMaster.Tests/Class1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 using NUnit.Framework;
 using Yaplex.RegexMaster.App;
 using Yaplex.RegexMaster.Business;
@@ -13,7 +14,7 @@ namespace Yaplex.RegexMaster.Tests
     public class Class1
     {
         [Test]
-        public void Should_Produce_Document()
+        public void Should_Produce_Document_Simple_word()
         {
             var source = "Hello world from my \"hello world\" application";
             var pattern = "hello";
@@ -21,6 +22,7 @@ namespace Yaplex.RegexMaster.Tests
             vm.Parse();
             Assert.AreEqual(source, vm.SourceText);
             Assert.AreEqual(pattern, vm.RegexPattern);
+            Assert.AreEqual(source, new TextRange(vm.ParsedDocument.ContentStart, vm.ParsedDocument.ContentEnd).Text.Trim());
         }
     }
 }
